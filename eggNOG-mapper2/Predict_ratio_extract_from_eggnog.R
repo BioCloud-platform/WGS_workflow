99b0e1c8db7fbad591aaa77693f58903ccd1c7bb
yuxiangtan <ytan7@bu.edu>
2023-04-28 21:56:27 +0800

    Create Predict_ratio_extract_from_eggnog.R

---
+++ eggNOG-mapper2/Predict_ratio_extract_from_eggnog.R
@@ -0,0 +1,85 @@
+#   Copyright {2023} Yuxiang Tan
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+library("stringr")
+
+#this script is to extract unannotated ratio from the eggnog annotation file
+
+#check arguments
+for (e in commandArgs()) {
+        ta = strsplit(e,"=",fixed=TRUE)
+        if(! is.na(ta[[1]][2])) {
+                temp = ta[[1]][2]
+                if(substr(ta[[1]][1],nchar(ta[[1]][1]),nchar(ta[[1]][1])) == "I") {
+                temp = as.integer(temp)
+                }
+        if(substr(ta[[1]][1],nchar(ta[[1]][1]),nchar(ta[[1]][1])) == "N") {
+                temp = as.numeric(temp)
+                }
+        assign(ta[[1]][1],temp)
+        } else {
+        assign(ta[[1]][1],TRUE)
+        }
+}
+
+#check whether file in is exist
+if (!exists("file_in")) {
+    stop("\n\nWarning: Usage: the parameter file_in is not given. \n\n")
+}
+
+if (!exists("sample_name")) {
+    stop("\n\nWarning: Usage: the parameter sample_name is not given. \n\n")
+}
+
+
+#check whether the file is exist
+if (!file.exists(file_in)) {
+    stop(paste("\nRscript Predict_ratio_extract_from_eggnog file_in=",file_in," \nWarning: Usage: the input matrix is not exist, please check the path. \n\n",sep=""))
+}
+
+
+#file_in <- "/data/Xianjinyuan/LD_lab/public_datasets/culturomics_datasets/public_MAGs/PRJEB39057_eggnog2/bin.1026.STRAINS.emapper.annotations"
+#sample_name <- "bin.1026"
+eggnog_tsv <- read.table(file_in, sep='\t',colClasses = "character",comment.char = "", quote="", header=FALSE) #quote is very important in reading functional annotation files
+
+COG_emp = subset(eggnog_tsv, eggnog_tsv[,"V7"] == "-")
+COG_emp_ann <- COG_emp[,7:21]
+check_emp <- function(list){
+    if(sum(list=="-")==15){
+        #sum==15 means all "-"
+        emp_stat=TRUE
+    }else{
+        emp_stat=FALSE
+    }
+    return(emp_stat)
+}
+
+emp_anno_rows<-sum(apply(COG_emp_ann,1,check_emp))
+
+hypothetical_rate <- emp_anno_rows/dim(eggnog_tsv)[1]
+
+out_line <- paste(sample_name,as.character(hypothetical_rate),sep="\t")
+
+cat(paste0(out_line,"\n"),file=paste0(file_in,".hypo_rate.tsv"))
+
+
+
+
+
+
+
+
+
+
+
